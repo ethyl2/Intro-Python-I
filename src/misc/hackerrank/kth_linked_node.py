4fad13ce6a4e5ea87f501b207cdc58e1cc77cbf9
Heather Nuffer <zzzuzz@gmail.com>
2020-06-05 10:07:37 -0600

    Adds my solutons for a couple of hackerranks

---
+++ src/misc/hackerrank/kth_linked_node.py
@@ -0,0 +1,60 @@
+"""
+Hackerrank test #1
+Given head of ll
+and int k
+
+Remove the kth node from the end of a linked list and return the head
+
+If k > len(ll)  return head without modifying anything
+
+Optimal is O(n) runtime and O(1) space.
+"""
+
+
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def removeKthLinkedListNode(head, k):
+    # edge cases
+    if head is None:
+        return None
+
+    # Determine length of ll
+    length = 1
+    curr = head
+    while curr.next:
+        length += 1
+        curr = curr.next
+    # print(length)
+
+    # If k > length:
+    # return head
+    if k > length:
+        print("k > length")
+        return head
+
+    # If k == length, return the next node
+    if k == length:
+        return head.next
+
+    # Progress until length - k - 1 This will get us to the node before the kth node from end.
+    distance = length - k - 1
+    # Reset curr
+    curr = head
+    while distance > 0:
+        curr = curr.next
+        distance -= 1
+    # Reassign next pointers to skip over kth node
+    curr.next = curr.next.next
+
+    return head
+
+
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node1.next = node2
+print(removeKthLinkedListNode(node1, 2).value)
